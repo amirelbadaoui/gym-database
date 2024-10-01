b1017a61af2501dd53bfe081170e8c41c1b89fa6
Amir Elbadaoui <aelbada@ncsu.edu>
2024-10-01 05:30:56 -0400

    [Project Creation] Created main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/** 
+  @file main.c
+  @brief The beginning
+
+  This file will soon contain the main executing component
+  of the database simulating program
+
+  @author Amir Elbadaoui
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  printf("It is 5:14AM\n");
+}
